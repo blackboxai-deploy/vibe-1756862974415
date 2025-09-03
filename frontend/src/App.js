bc4daccbbb50d72ea5204443ae004ed00d378883
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-03 06:59:46 +0530

    Add frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import { Toaster } from "./components/ui/toaster";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const API = `${BACKEND_URL}/api`;
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        <Toaster />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
